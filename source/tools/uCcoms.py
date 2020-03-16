00e0ba134a01c1048ab1f4ded4d12ba98aea0db6
Jamie Lee <jamielee@The-God-machine.local>
2020-03-16 12:06:26 +0000

    ucoms update

--- source/tools/uCcoms.py
+++ source/tools/uCcoms.py
@@ -2,6 +2,11 @@ import numpy as np
 from abc import ABCMeta, abstractmethod
 from threading import Thread, Event
 import logging
+import sys, os
+# if running from command line, need to append the parent directories to the PATH
+# so python knows where to find "source.file"
+sys.path.append(os.path.join(os.path.join(os.getcwd(), os.pardir),os.pardir))
+from source.tools.constants import *
 
 
 import RPi.GPIO as GPIO
@@ -26,6 +31,7 @@ class UCComms(Thread, metaclass=ABCMeta):
             return True
 
         except Exception as e:
+            self._log.warning("Could not configure device")
             self._log.warning(e)
             return False
 
@@ -42,15 +48,36 @@ class UCComms(Thread, metaclass=ABCMeta):
 class PiRadio(UCComms):
 
     _logname = 'PiRadio'
-    _log = logging.getlogger(f'{_logname}')
+    _log = logging.getLogger(f'{_logname}')
 
     def __init__(self):
         super().__init__()
+        self.pipes = [[0xE0,0xE0,0xF1,0xF1,0xE0], [0xCC, 0xCE,0xCC,0xCE,0xCC]]
+        
 
     def _configure(self) -> None:
 
+        self.radio = RF24(25,8)
+        self.radio.begin()
+        self.radio.openReadingPipe(1,bytes(self.pipes[1]))
+        self.radio.openWritingPipe(bytes(self.pipes[0]))
+        self.radio.setChannel(PI_RADIO_CHANNEL)
+        self.radio.eneableDynamicPayloads()
+        self.radio.setRetries(5,15)
+        self.radio.startListening()
 
 
+    def readData(self, channel=PI_RADIO_CHANNEL):
+        self.radio.setChannel(channel)
+        if self.radio.available():
+            recieved = self.radio.read(self.radio.getDynamicPayloadSize())
+            self._log.debug("Recieved data: {}".format(recieved.decode('utf-8')))
+            return recieved.decode('utf-8')
+
+    def sendData(self, channel=PI_RADIO_CHANNEL):
+        self.radio.stopListening()
+
+    
 
     
 
